c0628f53605323ad1bbb1df0b2eb740ce43b5cc6
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-08 23:39:48 +0630

    server google auth

---
+++ server/config/keys01.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:
+    "726965380490-2sdj6dvv5amad0bkrok0dkdppkqidt0a.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: "mongodb://stephen:password@ds151242.mlab.com:51242/emaily-dev",
+};
