4eec9130c75bcbdafab49f122d06f434fab6c5de
Dev Su Su <suphonemyint.la@gmail.com>
2023-03-15 00:07:34 +0630

    section8 Handling Payments

---
+++ server/config/keys02.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID:
+    "309790480390-qbve65ketmjeui5rnl8bolvj372jk8u5.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  // mongoURI:
+  //   "mongodb+srv://emailydev:ezErvGuOLLRz8Ksy@cluster0.jjmimtc.mongodb.net/node-blog?retryWrites=true&w=majority",
+  mongoURI:
+    "mongodb+srv://netninja:Y8HxPeseo98w27n4@nodetuts.okytrdd.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "jlkjfdklajfdajiejijijfdf",
+};
+
+// const uri =
+//     "mongodb+srv://<username>:<password>@firstcluster.4rc4s.mongodb.net/<dbname>?retryWrites=true&w=majority";
